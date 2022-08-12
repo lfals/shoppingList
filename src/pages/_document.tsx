2bf6258527d32139f39c2e47a14473804f5e8af4
Luis Amorim <luisfelipeamorim@hotmail.com>
2022-08-12 00:23:48 -0300

    SEO Config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-      <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" />
+        <meta property="og:title" content="To buy List" />
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta
+          property="og:description"
+          content="A lista de compras que faltava"
+        />
+        <meta property="og:url" content="https://tobuylist.fluma.dev/" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="627" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
