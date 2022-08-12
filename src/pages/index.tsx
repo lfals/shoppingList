d753e53fb3c5d5774447137465a3a5f6706792b7
Victor Martins <victor2007azevedo@hotmail.com>
2022-08-11 22:22:34 -0300

    list dashboard hover effect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Grid, GridItem, HStack, Stack, Text, Tooltip, VStack } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import Layout from '../components/layout.component';
@@ -88,64 +89,69 @@ const List: NextPage = () => {
           </HStack>
         </Box>
 
-        <Box maxW={'900px'} w="100%" mt={[4,6,8,10]} overflowX={'hidden'}>
+        <Box maxW={'900px'} w="100%" mt={[4, 6, 8, 10]} overflowX={'hidden'}>
           <HStack
             height={['60vh']}
             w={'100%'}
             overflowY={'auto'}
             justifyContent={'start'}
             gap={'24px'}
+            p={2}
             flexWrap={'wrap'}
             alignItems={'start'}
           >
             {
               listRecoil.map((list: any) => {
                 return (
-                  <Box
-                    key={list.id}
-                    p={5}
-                    bgColor="#20212C"
-                    borderRadius={'12px'}
-                    h={'200px'}
-                    w={['95%','95%','95%','200px']}
-                    style={{marginInlineStart: '0px'}}
-                  >
-                    <VStack justifyContent={'space-between'} h={'100%'}>
-                      <Tooltip hasArrow label={list.name} placement="top">
-                        <Text
-                          fontSize={'3xl'}
-                          textAlign='center'
-                          fontWeight="bold"
-                          textOverflow={'ellipsis'}
-                          whiteSpace="nowrap"
-                          overflow={'hidden'}
-                          w={'100%'}
-                        >
-                          {list.name}
-                        </Text>
-                      </Tooltip >
-                      <HStack w={'100%'} justifyContent={'space-between'}>
-                        <VStack alignItems={'start'}>
-                          <Text fontSize={['sm', 'md', 'lg']} textAlign='center'>
-                            Itens
+                  <Link href={`/${list.id}`} key={list.id}>
+                    <Box
+                      p={5}
+                      bgColor="#20212C"
+                      borderRadius={'12px'}
+                      h={'200px'}
+                      w={['95%', '95%', '95%', '200px']}
+                      style={{ marginInlineStart: '0px', cursor: 'pointer', transition: '0.5s' }}
+                      // _hover={{border: '2px solid #fff'}}
+                      _hover={{ boxShadow: '0px 0px 8px 2px #FFFFFF' }}
+                    >
+                      <VStack justifyContent={'space-between'} h={'100%'}>
+                        <Tooltip hasArrow label={list.name} placement="top">
+                          <Text
+                            fontSize={'3xl'}
+                            textAlign='center'
+                            fontWeight="bold"
+                            textOverflow={'ellipsis'}
+                            whiteSpace="nowrap"
+                            overflow={'hidden'}
+                            w={'100%'}
+                          >
+                            {list.name}
                           </Text>
-                          <Text style={{ marginTop: '0px' }} fontSize={['sm', 'md', 'lg']} textAlign='center' fontWeight="bold">
-                            {list.items.length}
-                          </Text>
-                        </VStack>
-                        <VStack alignItems={'start'}>
-                          <Text fontSize={['sm', 'md', 'lg']} textAlign='center'>
-                            Total
-                          </Text>
-                          <Text style={{ marginTop: '0px' }} fontSize={['md', 'lg', 'xl']} textAlign='center' fontWeight="bold">
-                            {treatCurrency(handlePriceSum(list.items), false)}
-                          </Text>
-                        </VStack>
-                      </HStack>
+                        </Tooltip >
+                        <HStack w={'100%'} justifyContent={'space-between'}>
+                          <VStack alignItems={'start'}>
+                            <Text fontSize={['sm', 'md', 'lg']} textAlign='center'>
+                              Itens
+                            </Text>
+                            <Text style={{ marginTop: '0px' }} fontSize={['sm', 'md', 'lg']} textAlign='center' fontWeight="bold">
+                              {list.items.length}
+                            </Text>
+                          </VStack>
+                          <VStack alignItems={'start'}>
+                            <Text fontSize={['sm', 'md', 'lg']} textAlign='center'>
+                              Total
+                            </Text>
+                            <Text style={{ marginTop: '0px' }} fontSize={['md', 'lg', 'xl']} textAlign='center' fontWeight="bold">
+                              {treatCurrency(handlePriceSum(list.items), false)}
+                            </Text>
+                          </VStack>
+                        </HStack>
 
-                    </VStack>
+                      </VStack>
 
-                  </Box>
+                    </Box>
+                  </Link>
+
                 )
               })
             }
